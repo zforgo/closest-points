620c80380b32970153b6039412ea71fdcb24b108
zforgo <zforgo@gmail.com>
2016-02-08 14:51:10 +0100

    Boost

--- src/main/java/hu/zforgo/gravity/ClosestPoints.java
+++ src/main/java/hu/zforgo/gravity/ClosestPoints.java
@@ -8,58 +8,32 @@ import javaslang.collection.List;
 import javaslang.collection.Stream;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
 import java.util.function.Function;
 
 public class ClosestPoints {
 
-	public static volatile ClosestPoints INSTANCE;
-	private ClosestPoints(){}
-
-	public static ClosestPoints getInstance() {
-		if (INSTANCE == null) {
-			INSTANCE = new ClosestPoints();
-		}
-		return INSTANCE;
-	}
-
-	private static List<String> readLines(Path path) {
-		final Scanner scanner;
-		try {
-			scanner = new Scanner(path.toFile());
-		} catch (FileNotFoundException e) {
-			return List.empty();
-		}
-		return List.ofAll(new Iterator<String>() {
-			@Override
-			public boolean hasNext() {
-				return scanner.hasNextLine();
-			}
-
-			@Override
-			public String next() {
-				return scanner.nextLine();
-			}
-		});
-	}
-
 	Function<String[], List<Double>> convert = (i) -> Stream.of(i).map(Double::parseDouble).toList();
 
 	Function<Tuple2<List<Double>, List<Double>>, Tuple3<List<Double>, List<Double>, Double>> calculate = (i) -> Tuple.of(
 			i._1,
 			i._2,
-			Math.sqrt(i._1.zip(i._2).map(t -> Math.pow(t._1 - t._2, 2)).sum().doubleValue())
+			i._1.zip(i._2).map(t -> Math.pow(t._1 - t._2, 2)).sum().doubleValue()
 	);
 
-	public Tuple3<List<Double>, List<Double>, Double> find(Path input) {
-		 return readLines(input)
+	public Tuple3<List<Double>, List<Double>, Double> find(Path input) throws IOException {
+		return Files.lines(input).collect(List.collector())
 				.map(s -> s.split("\t"))
 				.map(convert::apply)
 				.combinations(2)
+				.toJavaStream().parallel() // back to java.util.Stream which is parallel
 				.map(s -> Tuple.of(s.get(0), s.get(1)))
 				.map(calculate::apply)
-				.sort((o1, o2) -> o1._3.compareTo(o2._3))
-				.get(0);
+				.min((o1,o2)-> o1._3.compareTo(o2._3))
+				.get()
+				.transform((o1,o2,d)-> Tuple.of(o1,o2, Math.sqrt(d))); // Calculate sqrt only once
 	}
 }
